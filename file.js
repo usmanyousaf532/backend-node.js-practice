01088d8e5e99a3e6be192a3a33108659d5587091
Usman Yousaf <usman.yousaf@invozone.dev>
2026-02-10 01:14:53 +0500

    feat: file handling

---
+++ file.js
@@ -0,0 +1,45 @@
+const fs = require("fs");
+
+// sync file write
+
+// const res = fs.writeFileSync("./test.txt", "Hello World");
+// console.log(res);
+
+//async file write
+
+// fs.writeFile("./test.txt", "Hello Arham ", (err) => {});
+
+//sync file read
+
+// const result =(fs.readFileSync("./contacts.txt", "utf-8"));
+// console.log(result);
+
+// async file read
+
+// fs.readFile("./contacts.txt", "utf-8", (err, result) => {
+//   if (err) {
+//     console.log("error", err);
+//   } else {
+//     console.log(result);
+//   }
+// });
+
+// sync append on file
+
+// const res = fs.appendFileSync(
+//   "./test.txt",
+//   new Date().getDate().toLocaleString(),
+// );
+// console.log(res);
+
+// async append on file
+
+// fs.appendFile("./test.txt",)
+
+//copy file sync
+
+// fs.cpSync("./test.txt", "./copy.txt");
+
+// delete sync file
+
+// fs.unlinkSync("./copy.txt");
---
+++ hello.js
@@ -0,0 +1,6 @@
+console.log("hello world");
+console.log("hi");
+
+const { add, sub } = require("./math");
+
+console.log(add(6, 4));
