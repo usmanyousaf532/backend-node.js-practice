98ec2bf370e343727ae24401eeb16c7c4f968a25
Usman Yousaf <usman.yousaf@invozone.dev>
2026-02-12 15:15:47 +0500

    http req

---
+++ index.js
@@ -0,0 +1,12 @@
+// https
+
+const http = require("http");
+const fs = require("fs")
+const myServer = http.createServer((req, res) => {
+  console.log("New req res");
+  res.end("Hello from server");
+});
+
+myServer.listen(8000, () => {
+  console.log("server started");
+});
